8eed37ee3579396caea576489a09f5d9bddb5f41
Alexey Pargachev <pargachev20@gmail.com>
2023-02-25 15:26:11 +0700

    init

---
+++ cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"test-api/pkg/common/db"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	viper.SetConfigFile("./pkg/common/envs/.env")
+	viper.ReadInConfig()
+
+	port := viper.Get("PORT").(string)
+	dbUrl := viper.Get("DB_URL").(string)
+
+	r := gin.Default()
+	db.Init(dbUrl)
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"port":  port,
+			"dbUrl": dbUrl,
+		})
+	})
+
+	r.Run(port)
+}
