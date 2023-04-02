24b654f32b8c483d1e170479f952a1a4714553ff
Alexey Pargachev <pargachev20@gmail.com>
2023-04-02 19:27:52 +0700

    try add swagger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"test-api/pkg/books"
 	"test-api/pkg/common/config"
 	"test-api/pkg/common/db"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+
+	docs "test-api/cmd/server/docs"
+
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func init() {
@@ -18,26 +24,47 @@ func init() {
 	}
 }
 
+//	@title			Book API
+//	@version		1.0
+//	@description	Simple API GO + Gin
+
+// @host		localhost:8001
+// @BasePath	/api/v1
 func main() {
 	c := config.GetConfig() // Подлкючение конфига для получения переменных из .env
 
 	r := gin.Default()
+	docs.SwaggerInfo.BasePath = "/api/v1"
 
 	dbUrl := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", c.DbLogin, c.DbPassword, c.DbHost, c.DbPort, c.DbName)
 	fmt.Println(dbUrl)
 	h := db.Init(dbUrl)
 
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"port":  c.Port,
-			"dbUrl": c.DbHost,
-		})
-	})
+	r.GET("/", HealthCheck)
 
-	books.RegisterRoutes(r, h)
+	v1 := r.Group("/api/v1")
+	{
+		books.RegisterRoutes(v1, h)
+	}
 
 	port := fmt.Sprintf(":%s", c.Port)
 	fmt.Println("Listen on " + port)
 
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	r.Run(port)
+}
+
+// @Summary     Show the status of server.
+// @Description get the status of server.
+// @Tags        root
+// @Accept      */*
+// @Produce     json
+// @Success     200 {object} map[string]interface{}
+// @Router      / [get]
+func HealthCheck(c *gin.Context) {
+	res := map[string]interface{}{
+		"data": "Server is up and running",
+	}
+
+	c.JSON(http.StatusOK, res)
 }
--- pkg/books/controller.go
+++ pkg/books/controller.go
@@ -9,16 +9,18 @@ type handler struct {
 	DB *gorm.DB
 }
 
-func RegisterRoutes(r *gin.Engine, db *gorm.DB) {
+func RegisterRoutes(r *gin.RouterGroup, db *gorm.DB) {
 	h := &handler{
 		DB: db,
 	}
 
 	routes := r.Group("/books")
+	{
+		routes.POST("/", h.AddBook)
+		routes.GET("/", h.GetBooks)
+		routes.GET("/:id", h.GetBook)
+		routes.PUT("/:id", h.UpdateBook)
+		routes.DELETE("/:id", h.DeleteBook)
+	}
 
-	routes.POST("/", h.AddBook)
-	routes.GET("/", h.GetBooks)
-	routes.GET("/:id", h.GetBook)
-	routes.PUT("/:id", h.UpdateBook)
-	routes.DELETE("/:id", h.DeleteBook)
 }
